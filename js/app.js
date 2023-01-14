073b9b1aea58ff3381ea9b63b582053471622c3c
marcurious <radovic.b.marko@gmail.com>
2023-01-14 11:46:29 +0100

    alpha v0.1
    1st alpha release

---
+++ js/app.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+  // Initialize the Google SDK
+  gapi.load('auth2', function () {
+    gapi.auth2.init({
+      client_id: 'YOUR_CLIENT_ID',
+      scope: 'profile email'
+    });
+  });
+
+  // Attach a click event to the login button
+  document.getElementById('google-login-btn').addEventListener('click', function () {
+    // Get the Google auth instance
+    var auth2 = gapi.auth2.getAuthInstance();
+
+    // Sign the user in
+    auth2.signIn().then(function (user) {
+      // Send the user's ID token to your server
+      var id_token = user.getAuthResponse().id_token;
+      // Send the token to your server for verification and to create a session
+    });
+  });
+}

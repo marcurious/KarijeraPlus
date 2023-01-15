f421ccd7409540c2868aa7465c16a06391cbb9f6
marcurious <radovic.b.marko@gmail.com>
2023-01-15 17:33:57 +0100

    Update app.js
    Google integration - calling php scripts from

--- js/app.js
+++ js/app.js
@@ -1,22 +1,44 @@
-window.onload = function () {
-  // Initialize the Google SDK
-  gapi.load('auth2', function () {
-    gapi.auth2.init({
-      client_id: '238561646156-lc2hu8143guqlgrs82o9gdckomitqnub.apps.googleusercontent.com',
-      scope: 'profile email'
-    });
-  });
+<script>
+    gapi.load('auth2', function () {
+        gapi.auth2.init({
+            client_id: 'YOUR_CLIENT_ID',
+            scope: 'profile email'
+        });
 
-  // Attach a click event to the login button
-  document.getElementById('google-login-btn').addEventListener('click', function () {
-    // Get the Google auth instance
-    var auth2 = gapi.auth2.getAuthInstance();
-
-    // Sign the user in
-    auth2.signIn().then(function (user) {
-      // Send the user's ID token to your server
-      var id_token = user.getAuthResponse().id_token;
-      // Send the token to your server for verification and to create a session
+        document.getElementById('google-login-btn').addEventListener('click', function () {
+            var auth2 = gapi.auth2.getAuthInstance();
+            auth2.signIn().then(function (user) {
+                var id_token = user.getAuthResponse().id_token;
+                // Send the ID token to your server for verification
+                var xhr = new XMLHttpRequest();
+                xhr.open('POST', 'verify-google-token.php');
+                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                xhr.onload = function () {
+                    if (xhr.status === 200) {
+                        // The ID token is valid, create a session for the user
+                        var response = JSON.parse(xhr.responseText);
+                        createSession(response.userId);
+                    } else {
+                        console.log('An error occurred while verifying the ID token');
+                    }
+                };
+                xhr.send('id_token=' + id_token);
+            });
+        });
     });
-  });
-}
+
+    function createSession(userId) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'create-session.php');
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                // The session was created successfully, redirect the user to the dashboard
+                window.location.href = '/dashboard.php';
+            } else {
+                console.log('An error occurred while creating the session');
+            }
+        };
+        xhr.send('user_id=' + userId);
+    }
+</script>
